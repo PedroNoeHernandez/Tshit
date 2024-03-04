bd90dcc8bb30f122f1918a1390ca77d933539b27
pedro <pedronoe@comuinidad.unam.mx>
2024-03-04 16:39:37 +0000

    insert & delete

--- app/database/mysqlConn.py
+++ app/database/mysqlConn.py
@@ -14,5 +14,37 @@ class mysqlConn:
     def stop(self):
         self.cursor.close()
         self.connect.close()
+    
+### INSERT
+
+    def insert(self,table:str,values:list):
+        sql = f"INSERT INTO {table} VALUES ("
+        for i in range(0,len(values)-1):
+            sql = sql + " %s,"
+        sql= sql+ "%s)"
+        try:
+            self.cursor.execute(sql, values)
+            self.connect.commit()
+            return "Successfully inserted"
+        except (MySQLdb.Error, MySQLdb.Warning) as e:
+            return e
+        finally:
+            self.cursor.close()
+
+### DELETE
+    def delete(tabla:str, id:str):
+        sql = f"DELETE FROM {tabla} WHERE id ='{id}'"
+        try:
+            self.cursor.execute(sql)
+            self.connect.commit()
+        except (MySQLdb.Error, MySQLdb.Warning) as e:
+            return e
+        finally:
+            self.cursor.close()
+
+
+### GET ALL
+
+### SEARCH
 
     
